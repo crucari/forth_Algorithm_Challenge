5f1f3ed6b87bd69b141cdff1cd7792b0833ca1f8
Carina Cruz <lecruz@bu.edu>
2018-04-21 15:02:53 +0000

    notes

---
+++ fourthAlgo.c
@@ -0,0 +1,17 @@
+// Create the following algorithm function:
+// Should print a result then return an integer code
+// Function name: strContains
+
+// Inputs: 2 strings
+
+// If the 2nd string does not exist inside the first stringz then print:
+// “The string does not exist”
+// Then return the integer 0
+// If the 2nd string exists inside the first string print:
+// “The string exists”
+// Then return the integer 1
+// Ignore case.
+// Hacker level:
+// If the string contains the word more than once print:
+// “This string is full of the word”
+// Then return the integer 2
